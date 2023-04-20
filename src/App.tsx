6ef7daf129b0bc2e986273fb764d35b0c0e1f527
fhusain01 <fhusain@adaptready.com>
2023-04-20 20:08:10 +0530

    Created array input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
+import ArrayInput from './Components/ArrayInput';
 
 function App() {
   return (
     <div className="App">
       <h1>Fable Assignment</h1>
+
+      <ArrayInput />
     </div>
   );
 }
---
+++ src/Components/ArrayInput.tsx
@@ -0,0 +1,5 @@
+const ArrayInput = () => {
+  return null;
+};
+
+export default ArrayInput;
